77204cb92d2d128d6eea3fd096f9ca11481f17a1
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2021-12-03 23:09:33 +0000

    First commit

---
+++ Day 1/Part 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(@"/home/goncalo/Documents/adventToCode/input");
+        Int64? count = 0;
+        for (int i = 1; i < lines.Length; i++)
+        {
+            Int64 linha1 = Convert.ToInt64(lines[i - 1]);
+            Int64 linha2 = Convert.ToInt64(lines[i]);
+            if (linha1 < linha2)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
---
+++ Day 1/Part 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(@"/home/goncalo/Documents/adventToCode/input");
+        Int64? count = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            Int64 word1;
+            Int64 word2;
+            Int64 word3;
+            Int64 word4;
+            Int64 word5;
+            Int64 word6;
+            try {
+                word1 = Convert.ToInt64(lines[i]);
+                word2 = Convert.ToInt64(lines[i + 1]);
+                word3 = Convert.ToInt64(lines[i + 2]);
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                break; 
+            }
+            try {
+                word4 = Convert.ToInt64(lines[i + 1]);
+                word5 = Convert.ToInt64(lines[i + 2]);
+                word6 = Convert.ToInt64(lines[i + 3]);
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                break; 
+            }  
+
+            Int64 linha1 = word1 + word2 + word3;
+            Int64 linha2 = word4 + word5 + word6;
+            if (linha1 < linha2)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
