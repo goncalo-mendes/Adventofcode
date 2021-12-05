b6c8e3bc1c832f161d49f2eac1503aa5bcba37cd
Gonçalo Mendes <goncalo.mendes@ideiasdinamicas.com>
2021-12-05 22:26:28 +0000

    Day 2

---
+++ Day 2/Part1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(@"C:/Users/goncalo.mendes/RiderProjects/Adventofcode/Day 2/Part1/input.txt");
+        int count = 0;
+        int horizontal = 0;
+        int depth = 0;
+        
+        foreach (var text in lines)
+        {
+            string number = string.Empty;
+            string linha = Convert.ToString(text);
+            foreach (var t in linha)
+            {
+                if (Char.IsDigit(t))
+                    number += t;
+            }
+
+            if (linha.Contains("forward"))
+            {
+                horizontal += int.Parse(number);
+            }
+            else if (linha.Contains("up"))
+            {
+                depth -= int.Parse(number);
+            }
+            else if (linha.Contains("down"))
+            {
+                depth += int.Parse(number);
+            }
+        }
+
+        count = horizontal * depth;
+        Console.WriteLine(count);
+    }
+}
---
+++ Day 2/Part2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(@"C:/Users/goncalo.mendes/RiderProjects/Adventofcode/Day 2/Part1/input.txt");
+        int count = 0;
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+        foreach (var text in lines)
+        {
+            string number = string.Empty;
+            string linha = Convert.ToString(text);
+            foreach (var t in linha)
+            {
+                if (Char.IsDigit(t))
+                    number += t;
+            }
+
+            if (linha.Contains("forward"))
+            {
+                horizontal += int.Parse(number);
+                depth += int.Parse(number) * aim;
+            }
+            else if (linha.Contains("up"))
+            {
+                aim -=int.Parse(number);
+            }
+            else if (linha.Contains("down"))
+            {
+                aim +=int.Parse(number);
+            }
+        }
+
+        count = horizontal * depth;
+        Console.WriteLine(count);
+    }
+}
